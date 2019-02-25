7e352af6b3ce62b2f6e2b1a74f0804637d23ea5f
Blake Nelson <blake.nelson@tamu.edu>
2019-02-25 13:53:13 -0600

    bluetooth server make discoverable

--- blth/PyBluezServer.py
+++ blth/PyBluezServer.py
@@ -1,9 +1,11 @@
 import bluetooth
+import os
 
 hostMacAddress = 'b8:27:eb:b7:2a:37'
 port = 3
 backlog = 1
 size = 1024
+os.system("sudo hciconfig hci0 piscan")
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMacAddress, port))
 s.listen(backlog)
