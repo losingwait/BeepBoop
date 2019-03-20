3bfc9f77f37a0954982b2844235c5c604972e807
Amanda <absaibes@gmail.com>
2019-03-20 15:01:07 -0500

    multiple clients

--- blth/PyBluezServer.py
+++ blth/PyBluezServer.py
@@ -5,11 +5,18 @@ import threading
 def connectionHandler(client_sock):
 	size = 1024
 	print(client_sock)
-	while 1:
-		data = client_sock.recv(size)
-		if data:
-			print(data.decode())
-			client_sock.send(data)
+	try:
+		while server_alive:
+			data = client_sock.recv(size)
+			print("after sock recv")
+			if data and server_alive:
+				print(data.decode())
+		print("after while loop")
+		client_sock.close()
+	except:
+		print("[WARNING] Client socket closed, ending connection")
+		client_socks.remove(client_sock)
+		client_sock.close()
 
 
 host_mac_address = 'b8:27:eb:64:21:32'
@@ -20,13 +27,17 @@ os.system("sudo hciconfig hci0 piscan")
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((host_mac_address, port))
 s.listen(backlog)
-try:
-	client_sock, client_info = s.accept()
-	client_thread = threading.Thread(target=connectionHandler, args=(client_sock,))
-	client_thread.start()
+client_socks = []
+server_alive = True
 
+try:
+	while 1:
+		client_sock, client_info = s.accept()
+		client_socks.append(client_sock)
+		client_thread = threading.Thread(target=connectionHandler, args=(client_sock, ))
+		client_thread.start()
 	
 except:
-	print("closing socket")
-	client.close()
+	print("[WARNING] Closing Server, sending quit request to all clients")
+	server_alive = False
 	s.close()
