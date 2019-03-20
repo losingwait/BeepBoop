e8e5c4b0c928df11cd1f9a3b7dbe0ae34395c178
Amanda <absaibes@gmail.com>
2019-03-20 13:33:32 -0500

    first test for threading with blth connections

--- blth/PyBluezServer.py
+++ blth/PyBluezServer.py
@@ -1,21 +1,31 @@
 import bluetooth
 import os
+import threading
 
-hostMacAddress = 'b8:27:eb:64:21:32'
+def connectionHandler(client_sock):
+	size = 1024
+	print(client_sock)
+	while 1:
+		data = client_sock.recv(size)
+		if data:
+			print(data.decode())
+			client_sock.send(data)
+
+
+host_mac_address = 'b8:27:eb:64:21:32'
 port = 3
 backlog = 1
 size = 1024
 os.system("sudo hciconfig hci0 piscan")
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMacAddress, port))
+s.bind((host_mac_address, port))
 s.listen(backlog)
 try:
-	client, clientInfo = s.accept()
-	while 1:
-		data = client.recv(size)
-		if data:
-			print(data.decode())
-			client.send(data)
+	client_sock, client_info = s.accept()
+	client_thread = threading.Thread(target=connectionHandler, args=(client_sock,))
+	client_thread.start()
+
+	
 except:
 	print("closing socket")
 	client.close()
