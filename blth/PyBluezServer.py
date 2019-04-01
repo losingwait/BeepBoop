3439b5c9b4ea1ba95d39cc1b80523a0a0aa673f9
Amanda <absaibes@gmail.com>
2019-04-01 14:40:05 -0500

    polling status of station ids

--- blth/PyBluezServer.py
+++ blth/PyBluezServer.py
@@ -1,6 +1,7 @@
 import bluetooth
 import os
 import json
+import time
 import threading
 import requests
 
@@ -16,7 +17,27 @@ class Hub(object):
 		self.clients = {}
 		self.server_alive = True;
 		os.system("sudo hciconfig hci0 piscan")
+	
+	def pollStatus(self):
+		while True:
+			station_ids = []
+			for client in self.clients:
+				station_ids.append(client.encode())
+			data = {'station_list': station_ids}
+			print(data)
+			response = requests.get('https://losing-wait.herokuapp.com/machines/status', json = {'station_list': station_ids})
+			response_dict = self.convertJsonToDict(response.text)
+			print "Response from Polling is " + str(response_dict)
+			for station_id in response_dict:
+					self.clients[station_id.encode()]["client-sock"].send('P|' + response_dict[station_id.encode()])
+				
+			time.sleep(5)
 		
+	def convertJsonToDict(self, json_obj):
+		json_string = json_obj.replace("'", "\"")
+		json_dict = json.loads(json_string)
+		return json_dict
+	
 	def connectionHandler(self, client_sock, station_id):
 		print(client_sock)
 		client_sock.setblocking(False)
@@ -35,14 +56,14 @@ class Hub(object):
 							print("Shit sux yo")
 						else:
 							print("yEET")
-							json_string = response.text.replace("'", "\"")
-							
-							status = json.loads(json_string)
+							#~ json_string = response.text.replace("'", "\"")
+							#~ status = json.loads(json_string)
+							status = self.convertJsonToDict(response.text)
 							print status
 							if status['checkin'] == True and status['checkout'] == False:
-								client_sock.send("occupied")
+								client_sock.send("R|occupied")
 							elif status['checkin'] == False and status['checkout'] == True:
-								client_sock.send("open")
+								client_sock.send("R|open")
 								
 				except bluetooth.btcommon.BluetoothError:
 					pass
@@ -60,9 +81,11 @@ class Hub(object):
 				self.clients[station_id.decode()] = {"client-sock" : client_sock, "status": "open"}
 				client_thread = threading.Thread(target=self.connectionHandler, args=(client_sock, station_id.decode()))
 				client_thread.start()
+				polling_thread = threading.Thread(target=self.pollStatus)
+				polling_thread.start()
 
 		except KeyboardInterrupt:
-			print("[WARNING] Closing Server, sending quit request to all clients")
+			print("\n[WARNING] Closing Server, sending quit request to all clients")
 			for client in self.clients:
 				try:
 					self.clients[client]["client-sock"].send("QUIT")
