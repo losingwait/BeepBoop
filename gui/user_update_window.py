11b9b928d74965e1763de6961cf1c0654221008c
amandabsaibes <35813091+amandabsaibes@users.noreply.github.com>
2019-04-19 18:12:38 -0500

    Queueing (#9)
    * queue status and denied checks
    
    * updates to queueing
    
    * syntax issues
    
    * try except
    
    * client sends quit request when it is exited
    
    * forgot to commit the file lol
    
    * try except
    
    * Gym reader for gym check in and check out
    
    * changes for queueing
    
    * remove response status 300
    
    * changes to the user registration window
    
    * still need to change rfidreader
    
    * change from RFID reader to Simple reader
    
    * simple reader changes
    
    * inital update user rfid window

--- gui/user_update_window.py
+++ gui/user_update_window.py
@@ -23,20 +23,19 @@ class InformationDialog(Gtk.Dialog):
 		box.add(label)
 		self.show_all()
 
-class UserRegistration(Gtk.Window):
+class UpdateUser(Gtk.Window):
 	def __init__(self):
-		self.card_reader = reader.SimpleMFRC522()
+		self.card_reader = reader()
 		self.rfid_value = ''
-		self.name = ''
 		self.email = ''
 		self.password = ''
 		
-		Gtk.Window.__init__(self, title='Entry Example')
+		Gtk.Window.__init__(self, title='Update User RFID')
 		self.set_border_width(10)
-		self.set_default_size(400,200)
+		self.set_default_size(375,175)
 		header_bar = Gtk.HeaderBar()
 		header_bar.set_show_close_button(True)
-		header_bar.props.title = 'User Registration'
+		header_bar.props.title = 'Update User RFID'
 		self.set_titlebar(header_bar)
 		
 		box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
@@ -45,17 +44,6 @@ class UserRegistration(Gtk.Window):
 		listbox = Gtk.ListBox()
 		listbox.set_selection_mode(Gtk.SelectionMode.NONE)
 		box_outer.pack_start(listbox, True, True, 0)
-		
-		### Name ###
-		row = Gtk.ListBoxRow()
-		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		row.add(hbox)
-		name = Gtk.Label("Name", xalign=0)
-		self.entry_name = Gtk.Entry()
-		self.entry_name.set_text('')
-		hbox.pack_start(name, True, True, 0)
-		hbox.pack_start(self.entry_name, False, True, 0)
-		listbox.add(row)
 		
 		### Email ###
 		row = Gtk.ListBoxRow()
@@ -98,59 +86,62 @@ class UserRegistration(Gtk.Window):
 		row.add(hbox)
 		self.rfid_button = Gtk.Button(label="Get RFID")
 		self.rfid_button.connect('clicked', self.on_rfid_button_clicked)	
-		self.submit_button = Gtk.Button(label='Submit')
-		self.submit_button.connect("clicked", self.on_submit_button_clicked)
+		self.update_button = Gtk.Button(label='Update User')
+		self.update_button.connect("clicked", self.on_update_button_clicked)
 		hbox.pack_start(self.rfid_button, True, False, 0)
-		hbox.pack_start(self.submit_button, True, False, 0)
+		hbox.pack_start(self.update_button, True, False, 0)
 		listbox.add(row)
 		
 		
-	def on_submit_button_clicked(self, widget):
-		self.name = self.entry_name.get_text()
+	def on_update_button_clicked(self, widget):
 		self.email = self.entry_email.get_text()
 		self.password = self.entry_password.get_text()
 		
-		if self.name == '' or self.email == '' or self.password == '' or self.rfid_value == '':
+		if self.email == '' or self.password == '' or self.rfid_value == '':
 			incomplete_dialog = InformationDialog(self, "Incomplete information", "All the information must be filled out")
 			incomplete_response = incomplete_dialog.run()
 			if incomplete_response == Gtk.ResponseType.OK:
 				incomplete_dialog.destroy()
 			return
-		else:
-			response = requests.post('https://losing-wait.herokuapp.com/users/signup', data = {'name' : self.name, 'email' : self.email, 'password' : self.password, 'rfid' : self.rfid_value})
+		#~ else:
+			# Check that the email/password combination is correct
+			# CORRECT -> api call to update user's RFID value
+			# INCORRECT -> Dialog
 			
-			if response.status_code == 400:
-				# did not work
-				incomplete_request = InformationDialog(self, "User Exists", "The user/rfid already exists")
-				incomplete_request_response = incomplete_request.run()
-				if incomplete_request_response == Gtk.ResponseType.OK:
-					incomplete_request.destroy()
-					return
-			elif response.status_code == 200:
-				# request went through
-				complete_request = InformationDialog(self, "Complete Request", "The user was added to the gym")
-				complete_request_response = complete_request.run()
-				if complete_request_response == Gtk.ResponseType.OK:
-					complete_request.destroy()
+			#~ response = requests.post('https://losing-wait.herokuapp.com/users/signup', data = {'name' : self.name, 'email' : self.email, 'password' : self.password, 'rfid' : self.rfid_value})
+			
+			#~ if response.status_code == 400:
+				#~ # did not work
+				#~ incomplete_request = InformationDialog(self, "User Exists", "The user/rfid already exists")
+				#~ incomplete_request_response = incomplete_request.run()
+				#~ if incomplete_request_response == Gtk.ResponseType.OK:
+					#~ incomplete_request.destroy()
+					#~ return
+			#~ elif response.status_code == 200:
+				#~ # request went through
+				#~ complete_request = InformationDialog(self, "Complete Request", "The user was added to the gym")
+				#~ complete_request_response = complete_request.run()
+				#~ if complete_request_response == Gtk.ResponseType.OK:
+					#~ complete_request.destroy()
 		return
 
 	def on_rfid_button_clicked(self, widget):
 		print('Getting RFID value...')
-		card_id, _ = self.card_reader.read()
+		card_id, _ = self.reader.read()
 		while not card_id:
-			card_id, _ = self.card_reader.read()
+			card_id, _ = self.reader.read()
 
 		self.entry_rfid.set_text(str(card_id))
 		self.rfid_value = str(card_id)
 		
-#try:
-win = UserRegistration()
-win.connect('destroy', Gtk.main_quit)
-win.show_all()
-Gtk.main()
+try:
+	win = UpdateUser()
+	win.connect('destroy', Gtk.main_quit)
+	win.show_all()
+	Gtk.main()
 
-#except:
-#	win.close()
+except:
+	win.close()
 
 
 
